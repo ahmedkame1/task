b3e397bb76a2e772b569e610458cf357dbaa36e6
CS <CS@DESKTOP-RP9208I>
2025-03-04 22:42:19 +0200

    Add project files.

---
+++ task1/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello in Islam's Carpet Cleaning Service");
+            Console.WriteLine("$25 per small");
+            Console.WriteLine("$35 per large");
+            Console.WriteLine("please enter Number of small carpets:");
+            int smallCarpets = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(" please enter Number of large carpets:");
+            int lareCarpets = Convert.ToInt32(Console.ReadLine());
+            smallCarpets = smallCarpets * 25;
+            lareCarpets = lareCarpets * 35;
+            double cost = smallCarpets + lareCarpets;
+            double tax = (6 * cost) / 100;
+            Console.WriteLine($"total stamate {smallCarpets + lareCarpets + tax}");
+            Console.WriteLine("This estimate is valid for 30 days");
+        }
+    }
+}
